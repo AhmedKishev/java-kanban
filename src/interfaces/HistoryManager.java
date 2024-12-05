71c168b981b36af764116cfe9c69a18c60cd1b5d
User Ahmed <uchihamadara20488@gmail.com>
2024-12-06 01:05:50 +0300

    Убрать ограничения по просмотру задач, а также убрать дубликаты. Исправить код стайл

--- src/interfaces/HistoryManager.java
+++ src/interfaces/HistoryManager.java
@@ -2,12 +2,16 @@ package interfaces;
 
 import task.Task;
 import memory.Node;
+
 import java.util.ArrayList;
 
 
 public interface HistoryManager {
     public void add(Task task);
+
     public ArrayList<Task> getHistory();
+
     void remove(Long id);
+
     void removeNode(Node node);
 }
